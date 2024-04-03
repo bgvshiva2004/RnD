bd4bbe6f368db760d672367f7acb72a3238679c9
B.G.V.Shiva <118362268+bgvshiva2004@users.noreply.github.com>
2024-04-04 01:16:37 +0530

    added requirements.txt

--- RnD/home/admin.py
+++ RnD/home/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import table_info,project_details
+from .models import project_details
 # Register your models here.
-admin.site.register(table_info)
 
 @admin.register(project_details)
 class ProjectAdmin(admin.ModelAdmin):
